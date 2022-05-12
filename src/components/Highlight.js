3fee5ebc3836725088f35722184776d83fe4d54c
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-11 18:46:39 -0600

    Cleaned up Highlight.js comments

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,22 +1,7 @@
-import { useEffect, useRef, useState } from "react";
-// import HighlightCard from "./HighlightCard";
-
 function Highlight({ highlightedMovie }) {
-  // function buildHighlightCard() {
-  //   return (
-  //     <div className="highlightCard">
-  //       <img src={highlightedMovie.Poster} alt={highlightedMovie.Title} />
-  //       <h3>{highlightedMovie.Title}</h3>
-  //       <p>{highlightedMovie.Year}</p>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div>
       <h2>Highlighted Movie</h2>
-      {/* <div className="">{highlightedMovie ? buildHighlightCard() : null}</div> */}
-      {/* <div className="">{buildHighlightCard()}</div> */}
       <div className="highlightCard">
         <img src={highlightedMovie.Poster} alt={highlightedMovie.Title} />
         <h3>{highlightedMovie.Title}</h3>
