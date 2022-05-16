35af760c3086569eb9518c93558042e72608ab16
Dylan Raye-Leonard <30805924+drayeleo@users.noreply.github.com>
2022-05-16 01:18:10 -0600

    deleted template text

--- src/components/App.js
+++ src/components/App.js
@@ -58,28 +58,3 @@ App
   OwenWilsonFanPage
 
 */
-
-// template App:
-//
-// import logo from "./logo.svg";
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.\notifications.html
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
